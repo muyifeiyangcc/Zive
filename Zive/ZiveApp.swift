26fead19e5ca224729078c885b1437f5e2fc6101
zzy <2218847957@qq.com>
2026-04-22 15:41:58 +0800

    Initial Commit

---
+++ Zive/ZiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZiveApp.swift
+//  Zive
+//
+//  Created by yangyang on 2026/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ZiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
